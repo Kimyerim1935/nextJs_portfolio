9f98a725381d33cc6f2abd4cb3f6393ad12d3fcf
김예림 <kimyerim1935@naver.com>
2023-08-06 03:18:44 +0900

    mod: err

--- src/components/home/aboutMeAnimation.tsx
+++ src/components/home/aboutMeAnimation.tsx
@@ -1,11 +1,11 @@
 import Lottie from 'react-lottie-player';
-import AboutMe from 'public/aboutMe.json';
+import AboutAnimtaion from 'public/aboutMe.json';
 
 export default function AboutMeAnimation() {
     return (
         <Lottie
             loop
-            animationData={AboutMe}
+            animationData={AboutAnimtaion}
             play
             style={{width: '500px', height: '350px'}}
         />
