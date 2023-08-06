21fa3a4c710343e955fec0c0dd49bd7f97de170f
Rim <61038091+Kimyerim1935@users.noreply.github.com>
2023-08-06 10:13:06 +0900

    Update hero.tsx

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">안녕하세요
                     <br className="hidden lg:inline-block"/>
                 </h1>
-                <p className="mb-8 leading-relaxed">해삐한 하루를 보내고 있는 꿈 많은 프론트앤드 개발자<br/> 김예림입니다😀</p>
+                <p className="mb-8 leading-relaxed">하루하루가 해삐한 꿈 많은 프론트앤드 개발자<br/> 김예림입니다😀</p>
                 <div className="flex justify-center">
                     <Link href='https://denim-kookaburra-74c.notion.site/b2308e7117db4bc590b313243b516044?v=b209ad706cec4935acc1b39927137c40&pvs=4' target="_blank">
                         <span className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트 보러가기</span>
