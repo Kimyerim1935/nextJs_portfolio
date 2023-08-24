64e9417def183c3596215d8fe3797b69d9853193
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-08-24 10:03:43 +0900

    mod: dark mode font color

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -10,10 +10,10 @@ export default function Hero() {
                 <p className="mb-8 leading-relaxed">하루하루가 해삐한 꿈 많은 프론트앤드 개발자<br/> 김예림입니다😀</p>
                 <div className="flex justify-center">
                     <Link href='https://denim-kookaburra-74c.notion.site/b2308e7117db4bc590b313243b516044?v=b209ad706cec4935acc1b39927137c40&pvs=4' target="_blank">
-                        <span className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트 보러가기</span>
+                        <span className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg dark:text-slate-200">프로젝트 보러가기</span>
                     </Link>
                     <Link target="_blank" href='https://github.com/Kimyerim1935'>
-                        <span className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Github
+                        <span className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none rounded text-lg dark:hover:text-gray-600">Github
                         </span>
                     </Link>
                 </div>
