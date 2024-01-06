384e82942e99b88f3fe56b3f2f5c866e60e7d459
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2024-01-07 00:35:50 +0900

    mod: 코드 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,6 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isServerSide = typeof window === "undefined";
-
-  if (!isServerSide) return false;
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
