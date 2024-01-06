0f5d1d33af8760d7a5ac92af5dd90f797eebd068
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2024-01-07 00:16:38 +0900

    add: open graph

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css" rel="stylesheet"></link>
+        <meta property="og:title" content="<HappyRim/>" />
+        <meta property="og:description" content="Yerim's Portfolio" />
+        <meta property="og:url" content="https://next-js-portfolio-lake-eta.vercel.app/" />
+        <meta property="og:image" content="/coding.png" />
       </Head>
       <body>
         <Main />
