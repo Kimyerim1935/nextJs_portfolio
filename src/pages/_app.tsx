2e6c534a2848328c8d46801c358c16832496987b
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2024-01-26 23:45:21 +0900

    mod: GA 에러 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from 'next-themes';
 import { useEffect, useState } from 'react';
 import * as gtag from '../libs/gtags';
 import { useRouter } from 'next/router';
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isClient, setIsClient] = useState(false)
@@ -27,7 +29,30 @@ export default function App({ Component, pageProps }: AppProps) {
       return null
   }
   return (
-    <ThemeProvider attribute='class'>
+    <>
+    <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <ThemeProvider attribute='class'>
        <Component {...pageProps} />
-     </ThemeProvider>)
+     </ThemeProvider>
+    </>
+    )
 }
