488342d07c79c2f3db8da578c156fe9a48fce1fa
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2024-02-04 22:38:16 +0900

    add: ga 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,58 @@ import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isClient, setIsClient] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     setIsClient(true)
   }, [])
 
+    useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+    }, [router.events])
+
   if (!isClient) {
       return null
   }
   return (
+    <>
+     <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <ThemeProvider attribute='class'>
        <Component {...pageProps} />
      </ThemeProvider>
+    </>
+
     )
 }
